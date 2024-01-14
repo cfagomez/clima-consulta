fb944ee8a1621949c40a1bdcf80e3a1d34b4e830
Federico <cuervog992@gmail.com>
2024-01-14 17:49:40 -0300

    estilos resultados

--- src/components/Resultado.jsx
+++ src/components/Resultado.jsx
@@ -6,21 +6,36 @@ const Resultado = () => {
     const {main} = resultado
     const {temp, feels_like, temp_min, temp_max, pressure, humidity} = main
 
+    const kelvin = 273.15
+
   return (
     <div className="contenedor m-top">
-      <div>
-        <div className="flex-column">
-          <p>{temp}</p>
-          <p>{feels_like}</p>
+      <div className="contenedor-resultado">
+        <div className="contenedor-datos-principales">
+          <p className="p-temp-actual">{parseInt(temp - kelvin)}°C</p>
+          <p className="p-feels-like"><span>Sensación térmica:</span> {parseInt(feels_like - kelvin)}°C</p>
         </div>
-        <div>
-          <p>{temp_min}</p>
-          <p>{temp_max}</p>
+        <div className="contenedor-datos-secundarios">
+          <div>
+            <span>Máxima:</span>
+            <p>{parseInt(temp_min - kelvin)}°C</p>
+          </div>
+          <hr />
+          <div>
+            <span>Mínima:</span>
+            <p>{parseInt(temp_max - kelvin)}°C</p>
+          </div>
+          <hr />
+          <div>
+            <span>Presión:</span>
+            <p>{pressure}mb</p>
+          </div>
+          <hr />   
+          <div>
+            <span>Humedad:</span>
+            <p>{humidity}%</p>
+          </div>        
         </div>
-      </div>
-      <div>
-        <p>{pressure}</p>
-        <p>{humidity}</p>
       </div>
     </div>
   )
